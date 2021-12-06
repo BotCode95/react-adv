55f096c3a0b74460818b1d93c03e50aceb7bc4a2
PBottino <pbottino@axxonconsulting.com>
2021-12-06 19:56:45 -0300

    lazyload

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import { LazyPage1 } from '../pages';
 import LazyPage2 from '../pages/LazyPage2';
 import LazyPage3 from '../pages/LazyPage3';
@@ -14,18 +14,20 @@ export const LazyLayout = () => {
                     <NavLink to="lazy1">Lazy 1</NavLink>
                 </li>
                 <li>
-                    <NavLink to="lazy1">Lazy 2</NavLink>
+                    <NavLink to="lazy2">Lazy 2</NavLink>
                 </li>
                 <li>
-                    <NavLink to="lazy1">Lazy 3</NavLink>
+                    <NavLink to="lazy3">Lazy 3</NavLink>
                 </li>
             </ul>
         </div>
 
         <Routes>
             <Route path="lazy1" element={<LazyPage1/>}/>
-            <Route path="lazy1" element={<LazyPage2/>}/>
-            <Route path="lazy1" element={<LazyPage3/>}/>
+            <Route path="lazy2" element={<LazyPage2/>}/>
+            <Route path="lazy3" element={<LazyPage3/>}/>
+            {/* <Route path="*" element={<div>Not Found</div>}/> */}
+            <Route path="/*" element={<Navigate to="lazy1" replace/>}/>
         </Routes>
         </>
     )
